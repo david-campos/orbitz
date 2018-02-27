c284d675852516eb4b076303e34baa1e68ac1d4e
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-02-27 19:50:03 +0100

    Editado sonidos y scripts

--- scripts.js
+++ scripts.js
@@ -1,39 +1,41 @@
 function enOrbita(pl)
+// Comprueba
 {
 	for(var i in planetas)
 	{
-		var p = planetas[i];
-		if( (p.nodisponible && !pl.gravedad) || pl.planetaAnt == p) continue;
-		
-		//Vamos a obtener la menor distancia del segmento (pl.anterior_pos pl.pos) al centro del planeta
-		var d = menorDistanciaPS(pl.anterior_pos, pl, p);
-		if(d <= p.rg + radioJug && d >= p.rg - radioJug)
-			return p;
+		if(planetas.hasOwnProperty(i)) {
+            var p = planetas[i];
+            if ((p.nodisponible && !pl.gravedad) || pl.planetaAnt === p) continue;
+
+            //Vamos a obtener la menor distancia del segmento (pl.anterior_pos pl.pos) al centro del planeta
+            var d = menorDistanciaPS(pl.anterior_pos, pl, p);
+            if (d <= p.rg + radioJug && d >= p.rg - radioJug)
+                return p;
+        }
 	}
 	return null;
 }
 function menorDistanciaPS(A, B, C)
-//Menor distancia del segmento AB al punto C
+// Menor distancia del segmento AB al punto C
 {
-	var d = -1;
-	var uN = (C.x-A.x)*(B.x-A.x)+(C.y-A.y)*(B.y-A.y); //Numerador
-	
-    var uD = Math.pow((B.x - A.x),2)+Math.pow((B.y - A.y), 2); //Denominador
-	
-    if(uD == 0) console.log("DIVISION POR CERO EN menorDistanciaPS");
-	var u = uN / uD;
-	//Si u < 0, d = A->C; u > 1, d = B->C; else d = P->C (Calcular P)
+    if(A.x === B.x && A.y === B.y) {
+        // A y B son el mismo punto, devolvemos A->C
+        return moduloVector(A.x - C.x, A.y - C.y);
+    } else {
+        var uN = (C.x-A.x)*(B.x-A.x)+(C.y-A.y)*(B.y-A.y); //Numerador
+        var uD = Math.pow((B.x - A.x),2)+Math.pow((B.y - A.y), 2); //Denominador
+        var u = uN / uD;
+        //Si u < 0, d = A->C; u > 1, d = B->C; else d = P->C (Calcular P)
 
-	if(u < 0)
-		d = moduloVector(A.x - C.x, A.y - C.y);
-	else if(u > 1)
-		d = moduloVector(B.x - C.x, B.y - C.y);
-	else
-	{
-		var P = [A.x + u*(B.x - A.x), A.y + u * (B.y - A.y)];
-		d = moduloVector(P[0] - C.x, P[1] - C.y);
-	}
-	return d;
+        if (u < 0)
+            return moduloVector(A.x - C.x, A.y - C.y);
+        else if (u > 1)
+            return moduloVector(B.x - C.x, B.y - C.y);
+        else {
+            var P = [A.x + u * (B.x - A.x), A.y + u * (B.y - A.y)];
+            return moduloVector(P[0] - C.x, P[1] - C.y);
+        }
+    }
 }
 function signo(n)
 {
@@ -48,7 +50,7 @@ function moduloVector(x, y)
 
 function nota(mensaje, color)
 {
-	var miNota = new Object();
+	var miNota = {};
 	miNota.mensaje = mensaje;
 	miNota.t = Date.now() + 5000;
 	miNota.color = color;
--- sonidos.js
+++ sonidos.js
@@ -1,18 +1,19 @@
-var sonidos = new Object();
-sonidos.pong = new Audio('pong.ogg');
+var sonidos = {};
+sonidos.pong = new Audio('snd/pong.ogg');
 sonidos.pong.volume = 0.8;
-sonidos.pong2 = new Audio('pong2.ogg');
+sonidos.pong2 = new Audio('snd/pong2.ogg');
 sonidos.pong2.volume = 0.5;
-sonidos.entrada = new Audio('entrada.ogg');
+sonidos.entrada = new Audio('snd/entrada.ogg');
 sonidos.entrada.volume = 0.5;
-sonidos.claxon = new Audio('claxon.ogg');
+sonidos.claxon = new Audio('snd/claxon.ogg');
 sonidos.claxon.volume = 0.2;
-sonidos.cambio = new Audio('cambioOrbita.ogg');
+sonidos.cambio = new Audio('snd/cambioOrbita.ogg');
 sonidos.cambio.volume = 0.2;
-sonidos.muerte = new Audio('muerte.ogg');
+sonidos.muerte = new Audio('snd/muerte.ogg');
 sonidos.muerte.volume = 0.5;
 
-sonidos.fondo = new Audio('fondo.ogg');
+sonidos.fondo = new Audio('snd/fondo.ogg');
 sonidos.fondo.volume = 0.1;
 sonidos.fondo.loop = true;
+
 sonidos.fondo.play();
