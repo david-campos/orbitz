6eaa27d6e061893ab6f820c2314ca4645de67871
David Campos Rodríguez <caeucheunchinchoochan@msn.com>
2018-03-03 19:50:12 +0100

    Script para iniciar el deploy de forma sencilla
    JEJEJEJEJEJEJEJEOijaoijaoifjaofpoancjonak

---
+++ deploymentStep1.bat
@@ -0,0 +1,9 @@
+if exist "deploy\" rd /S /Q deploy
+mkdir deploy
+cat globals.js sonidos.js bola.js elementos.js scripts.js game.js generacion.js input.js log.js renderizado.js main.js menu.js > deploy\theScript.js
+cat css\cyborg.css css\estilos.css css\fonts.css > deploy\theCss.css
+xcopy /Y /I index.html deploy
+xcopy /Y /I favicon.ico deploy
+xcopy /S /I /Y css\fonts deploy\fonts
+xcopy /S /I /Y img deploy\img
+xcopy /S /I /Y snd deploy\snd
